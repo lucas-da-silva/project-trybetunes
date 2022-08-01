26299fea5de683d511563a287100786558def6a9
lucas-da-silva <lucassouza00012345@gmail.com>
2022-08-01 16:45:13 -0300

    second requirement done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -9,18 +9,35 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
+  state = {
+    isRedirect: false,
+  }
+
+  loginRedirect = () => {
+    this.setState({ isRedirect: true });
+  }
+
   render() {
+    const { isRedirect } = this.state;
+
     return (
       <div className="app">
         <p>TrybeTunes</p>
         <BrowserRouter>
           <Switch>
+            {
+              isRedirect && <Redirect to="/search" />
+            }
             <Route exact path="/album/:id" component={ Album } />
             <Route exact path="/search" component={ Search } />
             <Route exact path="/profile/edit" component={ ProfileEdit } />
             <Route exact path="/profile" component={ Profile } />
             <Route exact path="/favorites" component={ Favorites } />
-            <Route exact path="/" component={ Login } />
+            <Route
+              exact
+              path="/"
+              render={ () => <Login loginRedirect={ this.loginRedirect } /> }
+            />
             <Route component={ NotFound } />
           </Switch>
         </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const MIN_LENGTH_INPUT = 3;
 
 class Login extends Component {
+  state = {
+    lengthNameInput: 0,
+    valueNameInput: '',
+    loading: false,
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      lengthNameInput: value.length,
+      valueNameInput: value,
+    });
+  }
+
+  savingUser = async () => {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { loginRedirect } = this.props;
+        const { valueNameInput } = this.state;
+        await createUser({ name: valueNameInput });
+        loginRedirect();
+      },
+    );
+  }
+
   render() {
+    const { lengthNameInput, loading } = this.state;
+
+    let isDisabled = false;
+    if (lengthNameInput < MIN_LENGTH_INPUT) {
+      isDisabled = true;
+    }
+
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        {
+          loading ? <Loading /> : (
+            <form action="get">
+              <input
+                type="text"
+                name="login-name-input"
+                onChange={ this.handleChange }
+                id="login-name-input"
+                data-testid="login-name-input"
+              />
+              <button
+                type="button"
+                id="login-submit-button"
+                disabled={ isDisabled }
+                onClick={ this.savingUser }
+                data-testid="login-submit-button"
+              >
+                Entrar
+              </button>
+            </form>
+          )
+        }
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  loginRedirect: PropTypes.func.isRequired,
+};
 
 export default Login;
